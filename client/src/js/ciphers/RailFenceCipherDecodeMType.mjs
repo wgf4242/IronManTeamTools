20a1c7d68298076e968818be5359deefa5cf062a
wgf4242 <wgf4242@gmail.com>
2024-01-23 07:13:29 +0800

    sync

---
+++ client/src/js/ciphers/RailFenceCipherDecodeMType.mjs
@@ -0,0 +1,42 @@
+class RailFenceCipherTypeMDecode {
+
+    run(input, args) {
+        const [key, offset] = args;
+
+        const cipher = input;
+
+        if (key < 2) {
+            throw new OperationError("Key has to be bigger than 2");
+        } else if (key > cipher.length) {
+            throw new OperationError("Key should be smaller than the cipher's length");
+        }
+
+        if (offset < 0) {
+            throw new OperationError("Offset has to be a positive integer");
+        }
+
+        const cycle = (key - 1) * 2;
+        const plaintext = new Array(cipher.length);
+
+        let j = 0;
+        let x, y;
+
+        for (y = 0; y < key; y++) {
+            for (x = 0; x < cipher.length; x++) {
+                if ((y + x + offset + key - 1) % cycle === 0 || (y - (x + key - 1) - offset) % cycle === 0) {
+                    plaintext[x] = cipher[j++];
+                }
+            }
+        }
+
+        return plaintext.join("").trim();
+    }
+
+}
+
+export default RailFenceCipherTypeMDecode;
+
+// let decode = new RailFenceCipherTypeMDecode();
+// let res = decode.run("a2lg13f{}", [3, 0]);
+// console.assert(res === "flag{123}")
+// console.assert(decode.run("{a797g7441b553}aef9c6b2el7db607e5f3aae", [5, 0]) === "flag{7e73df4a49ba6c17b60a7b5952ee5e37}")
