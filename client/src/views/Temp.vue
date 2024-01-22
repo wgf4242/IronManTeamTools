401b80d1fe091cb8e5e29cd02fc0f2339a2eabd5
wgf4242 <wgf4242@gmail.com>
2024-01-23 07:13:24 +0800

    sync

--- client/src/views/Temp.vue
+++ client/src/views/Temp.vue
@@ -1,22 +1,24 @@
 <template>
-    <div>
-      <input v-model="enc" @keyup="submit" @change="submit">
-    </div>
-    <button @click="submit">Decrypt</button>
-    <table>
-      <tr v-for="(v, k, i) in plain">
-        <td>{{ k }}</td>
-        <td>{{ v }}</td>
-      </tr>
-    </table>
+  <div>
+    <input v-model="enc" @keyup="submit" @change="submit">
+  </div>
+  <button @click="submit">Decrypt</button>
+  <table>
+    <tr v-for="(v, k, i) in plain">
+      <td>{{ k }}</td>
+      <td>{{ v }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
-import { ref } from 'vue';
+import {ref} from 'vue';
+import RailFenceCipherTypeMDecode from "@/js/ciphers/RailFenceCipherDecodeMType.mjs";
 
 export default {
-  name: "Temp",
+  name: "TempComponent",
   setup() {
+    let railFenceM = new RailFenceCipherTypeMDecode();
     const enc = ref('');
     const plain = ref('')
     const copy = () => {
@@ -25,20 +27,39 @@ export default {
       if (!navigator.clipboard) {/* use old commandExec() way */
       } else {
         navigator.clipboard.writeText(text_to_copy)
-          .then(() => alert("copy to clipboard done!"))
-          .catch(() => alert("err"))
+            .then(() => alert("copy to clipboard done!"))
+            .catch(() => alert("err"))
       }
     }
+
+    function getRailFenceM(value) {
+      let result = ''
+      for (let i = 2; i < 10; i++) {
+        result += railFenceM.run(value, [i, 0]) + "\n";
+      }
+      return result.trim();
+    }
+
     const submit = () => {
+      let rail_fence_M_d_Value = getRailFenceM(enc.value);
+
       fetch('http://127.0.0.1:8000/test', {
         method: 'POST',
         body: enc.value
       }).then(res => {
         return res.json()
-      }).then(r => plain.value = r)
+      }).then(r => {
+        let obj = {...r, rail_fence_M_d: rail_fence_M_d_Value};
+        const sortedObj = {};
+        Object.keys(obj).sort().forEach(key => {
+          sortedObj[key] = obj[key];
+        });
+
+        plain.value = sortedObj;
+      })
     }
     console.log('123')
-    return { submit, copy, enc, plain }
+    return {submit, copy, enc, plain}
   }
 }
 </script>
