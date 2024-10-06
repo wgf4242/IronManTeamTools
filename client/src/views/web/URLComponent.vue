dc67e70f16d4dd5670f63cca9801dcedd493a9d7
wgf4242 <wgf4242@gmail.com>
2024-10-06 23:46:21 +0800

    sync

---
+++ client/src/views/web/URLComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const get_value = ref('')
+
+const url_encoding= computed(() => encodeURIComponent(get_value.value))
+
+
+</script>
+
+<template>
+  <p>Get 请求转码:</p>
+  <div> <textarea v-model="get_value"/></div>
+  <p>转码结果:</p>
+  <div> <textarea v-model="url_encoding"/></div>
+</template>
+
+<style scoped>
+
+textarea {
+  width: 80%;
+  height: 40px;
+}
+</style>
