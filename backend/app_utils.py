90576d605162b5b8260ccbb08f5784d20ad92f81
wgf4242 <wgf4242@gmail.com>
2025-01-01 12:19:53 +0800

    v2: before img update

--- backend/app_utils.py
+++ backend/app_utils.py
@@ -1,5 +1,14 @@
 from pathlib import Path
+def get_pictools_path():
+    file = Path(__file__)
+    is_debug = r'IronManTeamTools\backend' in f'{file.absolute()}'
+    if is_debug:
+        steg_base_path = Path(__file__).parent.parent / 'client/dist/tools'
+        pictools_path = steg_base_path / 'picture_tools'
+    else:
+        pictools_path = Path('.') / 'tools/picture_tools'
+    return str(pictools_path.absolute())
 
-a = Path(__file__)
-print('\\backend\\' in f'{a.absolute()}')
-print(r"\back\")
+
+if __name__ == '__main__':
+    print(get_pictools_path())
